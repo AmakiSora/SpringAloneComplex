8a97b845f681854cbeb2ec5cf0d3aa0bedcaa4cc
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2021-08-17 21:50:45 +0800

    SAC 1.10 Nacos服务管理

--- Order/src/main/java/com/cosmos/controller/OrderController.java
+++ Order/src/main/java/com/cosmos/controller/OrderController.java
@@ -14,7 +14,7 @@ public class OrderController {
     @GetMapping("/add")
     public String add(){
         System.out.println("下单成功");
-        String msg = restTemplate.getForObject("http://localhost:2022/stock/reduce", String.class);
+        String msg = restTemplate.getForObject("http://stock-service/stock/reduce", String.class);
         System.out.println(msg);
         return "下单成功"+msg;
     }
